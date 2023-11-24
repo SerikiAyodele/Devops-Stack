74f08763a45813628d9a22a36d335f34897788c9
Ayodele Seriki <serikiayodele@gmail.com>
2023-11-24 06:43:36 +0100

    script to generate random numbers

--- linux/bash-scripts/random-num.sh
+++ linux/bash-scripts/random-num.sh
@@ -1,6 +1,14 @@
-Lab 16.3: Using Random Numbers
+# Lab 16.3: Using Random Numbers
 
-Write a script which:
-- Takes a word as an argument.
-- Appends a random number to it.
-- Displays the answer.
+# Write a script which:
+# - Takes a word as an argument.
+# - Appends a random number to it.
+# - Displays the answer.
+
+#!/bin/bash
+
+echo "type in a word"
+read response
+
+echo "$response-$RANDOM"
+exit 0
