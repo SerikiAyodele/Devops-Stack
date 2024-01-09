3d35feb3a54eb59f98c557ed433de8649819602e
Ayodele Seriki <serikiayodele@gmail.com>
2024-01-09 17:54:20 +0100

    Lowest Common Ancestor of a Binary Search Tree

---
+++ python/grind-75/9-common-ancestors.py
@@ -0,0 +1,16 @@
+def commonAncestors(root, p, q):
+    curr= root
+
+    while curr:
+        if p.val > curr.val and q.val > curr.val:
+            curr = curr.right
+        elif p.val < curr.val and q.val < curr.val:
+            curr = curr.left
+        else:
+            return curr
+
+root = [6,2,8,0,4,7,9,3,5]
+p = 2
+q = 8
+result = commonAncestors(root, p, q)
+print(result)
